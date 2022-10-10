76006b19cfe0484315fe3ee55ebbf2bf3cbe0b42
Makabaka1880 <makabaka1880@outlook.com>
2022-10-10 10:26:05 +0800

    Initial Commit

---
+++ Classtime/ClasstimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClasstimeApp.swift
+//  Classtime
+//
+//  Created by SeanLi on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct ClasstimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
